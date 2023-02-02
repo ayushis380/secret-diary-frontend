889342385006bc2af279ddf59108f00f6cd8bf3d
AYUSHI SHARMA <ayushis380@gmail.com>
2023-02-01 21:16:31 -0700

    Login, Register form and styling changes

--- frontend-app/src/Register.jsx
+++ frontend-app/src/Register.jsx
@@ -1,5 +1,5 @@
 import React, {useState} from "react";
-export const Register = () => {
+export const Register = (props) => {
     const [email, setEmail] = useState('');
     const [pass, setPass] = useState('');
     const [name, setName] = useState('');
@@ -10,17 +10,18 @@ export const Register = () => {
     }
 
     return (
-        <>
-            <form onSubmit={handleSubmit}>
+        <div className="auth-form-container">
+            <h2>Register</h2>
+            <form className="register-form" onSubmit={handleSubmit}>
                 <label htmlFor="name">Full Name</label>
                 <input value= {name} name="name" id="name" placeholder="Full Name"></input>
                 <label htmlFor="email">Email</label>
                 <input value={email} onChange={(e) => setEmail(e.target.value)} type ="email" placeholder="youremail@gmail.com" id="email" name="email"></input>
                 <label htmlFor="password">Password</label>
                 <input value={pass} onChange={(e) => setPass(e.target.value)} type ="password" placeholder="********" id="password" name="password"></input>
-                <button type ="submit">Log In</button>
+                <button type ="submit">Create an account</button>
             </form>
-            <button>Already have an account? Login here</button>
-        </>
+            <button className="link-btn" onClick={() => props.onFormSwitch('login')}>Already have an account? Login here</button>
+        </div>
     )
 }
